aa9355f0c07b83ac9e60d703f58e5f0340c9253b
Elias-AAA <50029956@est.ort.edu.ar>
2026-06-27 00:02:44 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales"
    listo

---
+++ Assets/scripts/ContadorColeccionables.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContadorColeccionables : MonoBehaviour
+{
+    private int cantidadRecolectada = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coleccionable"))
+        {
+            cantidadRecolectada++;
+            Debug.Log("¡Laptop recolectada! Total: " + cantidadRecolectada);
+            
+            Destroy(other.gameObject);
+        }
+    }
+}
